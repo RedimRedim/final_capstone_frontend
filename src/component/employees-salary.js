17ed33bd62565c2a5e68ef73d62a21c4d6de1835
redim <redim@redim.com>
2024-11-13 15:13:34 +0700

    update api endpoint

--- src/component/employees-salary.js
+++ src/component/employees-salary.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NODE_API_URL;
+const API_URL = process.env.PYTHON_API_URL;
 import { cleaningDataInstance } from "../utils/datacleaning/clean";
 
 class EmployeesSalary {
